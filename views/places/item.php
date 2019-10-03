bcd073ba888af364684939b967faab2c53a15c61
reviakin@voider <reviakin@yamaldorstroy.ru>
2019-10-03 23:48:00 +0500

    Вывод полного пути помещений доработан теперь он собирается из ссылок на каждое помещение цепочки

--- views/places/item.php
+++ views/places/item.php
@@ -13,12 +13,25 @@ if (is_object($model)) {
 
 ?>
 <span class="places-item" >
-	<?= Html::a(
-        (isset($short))?$model->short:(isset($full)?$model->fullName:$model->name),
-        ['/places/view','id'=>$model->id],
-        ['qtip_ttip'=>$model->name,]
-        )
-    ?>
+    <?php if (isset($full)) {
+        $tokens=[];
+        $item=$model;
+        do {
+            $tokens[]=Html::a(
+                $item->short,
+                ['/places/view','id'=>$item->id],
+                ['qtip_ttip'=>$item->name,]
+            );
+            $item=\app\models\Places::fetchItem($item->parent_id);
+        } while (is_object($item));
+        echo implode('/',array_reverse($tokens));
+    } else {
+	    echo Html::a(
+		    isset($short)?$model->short:$model->name,
+            ['/places/view','id'=>$model->id],
+            ['qtip_ttip'=>$model->name,]
+        );
+    } ?>
 </span>
 
 <?php } else echo "Отсутствует";
