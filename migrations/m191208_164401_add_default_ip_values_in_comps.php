e14149f1735cde2fdef9d826de0a49d582e1ddf9
reviakin@voider <reviakin@yamaldorstroy.ru>
2019-12-15 14:08:31 +0500

    добавлено пустое значение IP по умолчанию. Иначе не завести ОС без IP

---
+++ migrations/m191208_164401_add_default_ip_values_in_comps.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191208_164401_add_default_ip_values_in_comps
+ */
+class m191208_164401_add_default_ip_values_in_comps extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+	    $this->alterColumn('comps','ip',$this->string(255)->null());
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+	    $this->alterColumn('comps','ip',$this->string(255)->notNull());
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191208_164401_add_default_ip_values_in_comps cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
