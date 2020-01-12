c382b5dea9466875b26ea9859f5798652ae9ab4c
reviakin@voider <reviakin@yamaldorstroy.ru>
2020-01-12 13:46:12 +0500

    добавлен автоинкремент к ключу пользователей

---
+++ migrations/m191208_173041_fix_users_id.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191208_173041_fix_many_2_many
+ */
+class m191208_173041_fix_users_id extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+    	$table=$this->db->getTableSchema('{{%users}}');
+	    if (!$table->getColumn('id')->autoIncrement) {
+	    	if (!count($table->primaryKey)) {
+			    $this->addPrimaryKey('id','users','id');
+		    }
+		    $this->alterColumn('users','id',$this->integer()->notNull()->append('AUTO_INCREMENT'));
+	    }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+	    $this->alterColumn('users','id',$this->integer()->notNull());
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191208_173041_fix_many_2_many cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
