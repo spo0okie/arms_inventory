981cce574efc2c2283074c5050fe9a5697f402ac
reviakin@voider <reviakin@yamaldorstroy.ru>
2020-01-12 13:24:24 +0500

    добавляет автоинкремент к материалам

---
+++ migrations/m191219_100001_fix_materials_id.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m191208_173041_fix_many_2_many
+ */
+class m191219_100001_fix_materials_id extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+    	$table=$this->db->getTableSchema('{{%materials}}');
+	    if (!$table->getColumn('id')->autoIncrement) {
+	    	if (!count($table->primaryKey)) {
+			    $this->addPrimaryKey('id','materials','id');
+		    }
+		    $this->alterColumn('materials','id',$this->integer()->notNull()->append('AUTO_INCREMENT'));
+	    }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+	    $this->alterColumn('materials','id',$this->integer()->notNull());
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m191208_173041_fix_many_2_many cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
