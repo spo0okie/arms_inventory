9fe8ea4092babbfbdc19bd38d0c2dd7898354b90
reviakin@voider <reviakin@yamaldorstroy.ru>
2020-01-14 09:05:56 +0500

    добавлена иконка человечек/шестеренка обозначающая потребителя сервиса

--- views/services/item.php
+++ views/services/item.php
@@ -3,15 +3,21 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\Places */
+/* @var $model app\models\Services */
 
-//qtip_ajxhrf="<?= \yii\helpers\Url::to(['/org-phones/ttip','id'=>$model->id])
-//qtip_class="qtip-wide"
-//	<?= Html::a('<span class="glyphicon glyphicon-pencil"/>',['/places/update','id'=>$model->id])
 
 if (is_object($model)) { ?>
 	<span class="services-item" qtip_ajxhrf="<?= \yii\helpers\Url::to(['/services/ttip','id'=>$model->id]) ?>">
-		<?= Html::a($model->name,['/services/view','id'=>$model->id]) ?>
-    	<?= Html::a('<span class="glyphicon glyphicon-pencil"/>',['/services/update','id'=>$model->id]) ?>
+		<?= Html::a(
+				'<small>'.
+			(
+					$model->is_end_user?
+					'<span class="glyphicon glyphicon-user"/></span>':
+					'<span class="glyphicon glyphicon-cog"/></span>'
+			).'</small>&nbsp;'.
+			$model->name,
+			['/services/view','id'=>$model->id]
+		) ?>
+    	<?= Html::a('<span class="glyphicon glyphicon-pencil"></span>',['/services/update','id'=>$model->id]) ?>
 	</span>
 <?php }
