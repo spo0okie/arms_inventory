a85ba5fa41152f278c486c46d7316a44ef8cad9e
reviakin@voider <reviakin@yamaldorstroy.ru>
2020-02-10 15:29:05 +0500

    поиск сервисов

---
+++ models/ServicesSearch.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Services;
+
+/**
+ * ServicesSearch represents the model behind the search form of `app\models\Services`.
+ */
+class ServicesSearch extends Services
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'is_end_user', 'user_group_id', 'sla_id'], 'integer'],
+            [['name', 'description', 'links', 'notebook'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Services::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+	        'pagination' => ['pageSize' => 100,],
+	        'sort'=> ['defaultOrder' => ['name'=>SORT_ASC]]
+        ]);
+	
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'is_end_user' => $this->is_end_user,
+            'user_group_id' => $this->user_group_id,
+            'sla_id' => $this->sla_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'name', $this->name])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'links', $this->links])
+            ->andFilterWhere(['like', 'notebook', $this->notebook]);
+
+        return $dataProvider;
+    }
+}
