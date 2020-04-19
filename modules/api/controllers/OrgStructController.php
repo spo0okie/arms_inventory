984aca5281e006b102ad96c1ce3676d9b0200338
reviakin@voider <reviakin@yamaldorstroy.ru>
2020-04-19 15:47:24 +0500

    API для подразделений

---
+++ modules/api/controllers/OrgStructController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: spookie
+ * Date: 19.04.2020
+ * Time: 15:41
+ */
+
+namespace app\modules\api\controllers;
+use app\models\OrgStruct;
+
+
+class OrgStructController extends \yii\rest\ActiveController
+{
+	public $modelClass='app\models\OrgStruct';
+	
+}
