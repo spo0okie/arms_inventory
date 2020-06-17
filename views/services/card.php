672b90e86c9b39bae3feb4a32a2c4a9c8434f1d3
reviakin@voider <reviakin@yamaldorstroy.ru>
2020-06-18 00:36:19 +0500

    добавлена частичная тестовая интеграция с dokuwiki

--- views/services/card.php
+++ views/services/card.php
@@ -29,94 +29,100 @@ $deleteable=!count($comps)&&!count($services)&&!count($dependants)&&!count($supp
 		</span>
 	<?php } ?>
 </h1>
-<h4>
-    (<?php
-		echo $model->is_end_user?'Предоставляется пользователям':'Внутренний сервис';
-		if (is_object($model->segment)) echo " // Сегмент ИТ: {$model->segment->name}"
-	?>)
-</h4>
-<?php
-$schedules=[];
-if (!empty($model->providingSchedule))
-	$schedules[]='<strong>Предоставляется:</strong> '.$model->providingSchedule->name;
-
-if (!empty($model->supportSchedule))
-	$schedules[]='<strong>Поддерживается:</strong> '.$model->supportSchedule->name;
-
-if (count($schedules)) {
-	echo implode('; ',$schedules).'<br />';
-}
-?>
-
-<?php if(!$static_view&&!$deleteable) { ?>
-    <p>
-    
-    </p>
-<?php } ?>
-<br />
-<p>
-	<?= Yii::$app->formatter->asNtext($model->description) ?>
-</p>
-<?= \app\components\UrlListWidget::Widget(['list'=>$model->links]) ?>
-<br />
-
-<h4>
-	Ответственный: <?= $this->render('/users/item',['model'=>$model->responsible,'static_view'=>$static_view]) ?>
-</h4>
-<?php if (count($support)) { ?>
-	При поддержке:
-    <p>
-    <?php
-        foreach ($support as $user)
-            echo $this->render('/users/item',['model'=>$user,'static_view'=>$static_view]).'<br />';
-    ?>
-    </p>
-    <br />
-<?php } ?>
-
-<?php if (count($comps)) { ?>
-	<h4>Выполняется на компьютерах:</h4>
-	<p>
+<div class="row">
+	<div class="col-md-6">
+		<h4>
+			(<?php
+			echo $model->is_end_user?'Предоставляется пользователям':'Внутренний сервис';
+			if (is_object($model->segment)) echo " // Сегмент ИТ: {$model->segment->name}"
+			?>)
+		</h4>
 		<?php
-		foreach ($comps as $comp)
-			echo $this->render('/comps/item',['model'=>$comp,'static_view'=>$static_view]).'<br />';
+		$schedules=[];
+		if (!empty($model->providingSchedule))
+			$schedules[]='<strong>Предоставляется:</strong> '.$model->providingSchedule->name;
+		
+		if (!empty($model->supportSchedule))
+			$schedules[]='<strong>Поддерживается:</strong> '.$model->supportSchedule->name;
+		
+		if (count($schedules)) {
+			echo implode('; ',$schedules).'<br />';
+		}
 		?>
-	</p>
-	<br />
-<?php } ?>
+		
+		<?php if(!$static_view&&!$deleteable) { ?>
+			<p>
 
-<?php if (count($techs)) { ?>
-	<h4>Выполняется на оборудовании:</h4>
-	<p>
-		<?php
-		foreach ($techs as $tech)
-			echo $this->render('/techs/item',['model'=>$tech,'static_view'=>$static_view]).'<br />';
-		?>
-	</p>
-	<br />
-<?php } ?>
+			</p>
+		<?php } ?>
+		<br />
+		<p>
+			<?= Yii::$app->formatter->asNtext($model->description) ?>
+		</p>
+		<?= \app\components\UrlListWidget::Widget(['list'=>$model->links]) ?>
+		<br />
 
-<?php if (count($services)) { ?>
-    <h4>Зависит от сервисов:</h4>
-    <p>
-		<?php
-		foreach ($services as $service)
-			echo $this->render('/services/item',['model'=>$service,'static_view'=>$static_view]).'<br />';
-		?>
-    </p>
-    <br />
-<?php } ?>
+		<h4>
+			Ответственный: <?= $this->render('/users/item',['model'=>$model->responsible,'static_view'=>$static_view]) ?>
+		</h4>
+		<?php if (count($support)) { ?>
+			При поддержке:
+			<p>
+				<?php
+				foreach ($support as $user)
+					echo $this->render('/users/item',['model'=>$user,'static_view'=>$static_view]).'<br />';
+				?>
+			</p>
+			<br />
+		<?php } ?>
 
-<?php if (count($dependants)) { ?>
-    <h4>Зависимые сервисы:</h4>
-    <p>
-		<?php
-		foreach ($dependants as $service)
-			echo $this->render('/services/item',['model'=>$service,'static_view'=>$static_view]).'<br />';
-		?>
-    </p>
-    <br />
-<?php } ?>
+	</div>
+	<div class="col-md-6">
+		<?php if (count($comps)) { ?>
+			<h4>Выполняется на компьютерах:</h4>
+			<p>
+				<?php
+				foreach ($comps as $comp)
+					echo $this->render('/comps/item',['model'=>$comp,'static_view'=>$static_view]).'<br />';
+				?>
+			</p>
+			<br />
+		<?php } ?>
+		
+		<?php if (count($techs)) { ?>
+			<h4>Выполняется на оборудовании:</h4>
+			<p>
+				<?php
+				foreach ($techs as $tech)
+					echo $this->render('/techs/item',['model'=>$tech,'static_view'=>$static_view]).'<br />';
+				?>
+			</p>
+			<br />
+		<?php } ?>
+		
+		<?php if (count($services)) { ?>
+			<h4>Зависит от сервисов:</h4>
+			<p>
+				<?php
+				foreach ($services as $service)
+					echo $this->render('/services/item',['model'=>$service,'static_view'=>$static_view]).'<br />';
+				?>
+			</p>
+			<br />
+		<?php } ?>
+		
+		<?php if (count($dependants)) { ?>
+			<h4>Зависимые сервисы:</h4>
+			<p>
+				<?php
+				foreach ($dependants as $service)
+					echo $this->render('/services/item',['model'=>$service,'static_view'=>$static_view]).'<br />';
+				?>
+			</p>
+			<br />
+		<?php } ?>
+	</div>
+</div>
 
 <?php if (!$static_view && strlen($model->notebook)) { ?>
     <h4>Записная книжка:</h4>
