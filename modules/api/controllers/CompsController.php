46d414c97314a0f9078f466257ab1f4a7b25c5a8
reviakin@yoga14 <reviakin@gmail.com>
2021-02-11 13:58:53 +0500

    добавлен поиск компа по IP

--- modules/api/controllers/CompsController.php
+++ modules/api/controllers/CompsController.php
@@ -18,16 +18,38 @@ class CompsController extends \yii\rest\ActiveController
         return $actions;
     }
     
-    public function actionSearch($domain,$name){
-        if (is_null($domainObj=Domains::find()->where(['name'=>strtoupper($domain)])->one()))
-            throw new \yii\web\NotFoundHttpException("Domain '$domain' not found");
-        $modelClass = $this->modelClass;
-        $model = $modelClass::find()->where([
-            'name' => strtoupper($name),
-            'domain_id' => $domainObj->id, 
-        ])->one();
+    public function actionSearch($domain=null,$name,$ip=null){
+		$modelClass = $this->modelClass;
+		$notFoundDescription="Comp with name '$name'";
+		$query=$modelClass::find()->where(['name'=>strtoupper($name)]);
+
+	
+		//добавляем фильтрацию по IP если он есть
+		if (!is_null($ip)) {
+			$query->andFilterWhere(['like','ip',$ip]);
+			$notFoundDescription.=" with IP $ip";
+		}
+	
+		$notFoundDescription.=" not found";
+		
+		//добавляем фильтрацию по домену если он есть
+    	if (!is_null($domain)) {
+			if (is_null($domainObj=Domains::find()
+				->where(['name'=>strtoupper($domain)])
+				->one()
+			))
+				throw new \yii\web\NotFoundHttpException("Domain '$domain' not found");
+			$query->andFilterWhere(['domain_id'=>$domainObj->id]);
+			$notFoundDescription.=" in domain $domain";
+		}
+	
+		
+		
+        $model = $query->one();
+        
+        
         if ($model === null)
-            throw new \yii\web\NotFoundHttpException("Comp with name '$name' not found in domain $domain (".$domainObj->id.")");
+            throw new \yii\web\NotFoundHttpException($notFoundDescription);
                 
             return $model;
     }
