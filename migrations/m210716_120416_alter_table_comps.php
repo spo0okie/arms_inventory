d0a93017c19467baf57446598a244385b4707aa8
reviakin@yoga14 <reviakin@gmail.com>
2021-07-16 17:13:36 +0500

    поле IP для ОС увеличено до 512байт т.к. есть случаи, когда не влезло

---
+++ migrations/m210716_120416_alter_table_comps.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m210716_120416_alter_table_comps
+ */
+class m210716_120416_alter_table_comps extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+		$this->alterColumn('comps','ip',$this->string(512));
+		$this->alterColumn('comps','ip_ignore',$this->string(512));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+		$this->alterColumn('comps','ip',$this->string(256));
+		$this->alterColumn('comps','ip_ignore',$this->string(256));
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m210716_120416_alter_table_comps cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
