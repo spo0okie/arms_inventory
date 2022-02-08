a14ed81bfa9e128238c83c11b174fdf0198b6d7d
reviakin@yoga14 <reviakin@gmail.com>
2022-02-08 13:11:53 +0500

    поддержка неск. внутренних номеров сотрудника

--- modules/api/controllers/PhonesController.php
+++ modules/api/controllers/PhonesController.php
@@ -42,25 +42,26 @@ class PhonesController extends \yii\rest\ActiveController
 		//ищем пользователя
 		$user = \app\models\Users::findOne($id);
 		//если нашли
+		$return=[];
 		if (is_object($user)){
 			//он прикреплен к АРМ?
 			$arms=$user->arms;
 			if (is_array($arms)) {
-				if (count($arms)>1) {
-
-				}
 				//перебираем армы
 				foreach ($arms as $arm){
 				//ищем у них телефоны
 				$phones=$arm->voipPhones;
 				if (is_array($phones)) foreach ($phones as $phone) {
-					if (strlen($phone->comment) && (int)$phone->comment) return $phone->comment;
+					if (strlen($phone->comment) && (int)$phone->comment)
+						$return[(int)$phone->comment]=(int)$phone->comment;
 				}
 			}}
 			if (is_object($techs=$user->techs)) foreach ($techs as $tech)
 				if ($tech->isVoipPhone && strlen($tech->comment) && (int)$tech->comment) {
-				return $tech->comment;
+					$return[(int)$tech->comment]=(int)$tech->comment;
 			}
+			if (count($return))
+				return implode(', ',$return);
 		}
 		throw new \yii\web\NotFoundHttpException("not found");
 	}
