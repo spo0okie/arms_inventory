08eec89971d9e530436aefe924bc7926a5b26df7
reviakin@yoga14 <reviakin@gmail.com>
2022-04-05 23:16:59 +0500

    phone search api добавлен поиск телефона по логину

--- modules/api/controllers/PhonesController.php
+++ modules/api/controllers/PhonesController.php
@@ -38,19 +38,27 @@ class PhonesController extends \yii\rest\ActiveController
 		throw new \yii\web\NotFoundHttpException("not found");
 	}
 
-	public function actionSearchByUser($id){
+	public function actionSearchByUser($id=null,$login=null){
 		//ищем пользователя
-		$user = \app\models\Users::findOne($id);
+		if ($id)
+			$user = \app\models\Users::findOne($id);
+		elseif ($login)
+			$user = \app\models\Users::find()
+			->where(['Login'=>$login])
+			->one();
 		//если нашли
+		//var_dump($user);
 		$return=[];
 		if (is_object($user)){
 			//он прикреплен к АРМ?
 			$arms=$user->arms;
+			//var_dump($arms);
 			if (is_array($arms)) {
 				//перебираем армы
 				foreach ($arms as $arm){
 				//ищем у них телефоны
 				$phones=$arm->voipPhones;
+				//var_dump($phones);
 				if (is_array($phones)) foreach ($phones as $phone) {
 					if (strlen($phone->comment) && (int)$phone->comment)
 						$return[(int)$phone->comment]=(int)$phone->comment;
@@ -62,7 +70,9 @@ class PhonesController extends \yii\rest\ActiveController
 			}
 			if (count($return))
 				return implode(', ',$return);
-		}
+			else
+				return $user->Phone;
+		} else
 		throw new \yii\web\NotFoundHttpException("not found");
 	}
 
