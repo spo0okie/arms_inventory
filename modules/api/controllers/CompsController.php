604939224e698484f4b086d6897956a61f7b7e56
reviakin@yoga14 <reviakin@gmail.com>
2022-05-31 13:19:25 +0500

    rest api comps index with auth

--- modules/api/controllers/CompsController.php
+++ modules/api/controllers/CompsController.php
@@ -7,13 +7,29 @@ use app\models\Domains;
 
 class CompsController extends \yii\rest\ActiveController
 {
-    
-    public $modelClass='app\models\Comps';
+	public function behaviors()
+	{
+		$behaviors=parent::behaviors();
+		if (!empty(\Yii::$app->params['useRBAC'])) $behaviors['access']=[
+			'class' => \yii\filters\AccessControl::className(),
+			'rules' => [
+				['allow' => true, 'actions'=>['index'], 'roles'=>['editor']],
+				['allow' => true, 'actions'=>['create','view','update','search'], 'roles'=>['@','?']],
+			],
+			'denyCallback' => function ($rule, $action) {
+				throw new  \yii\web\ForbiddenHttpException('Access denied');
+			}
+		];
+		return $behaviors;
+	}
+	
+	
+	public $modelClass='app\models\Comps';
     
     public function actions()
     {
         $actions = parent::actions();
-		unset($actions['index']);
+		//unset($actions['index']);
 		$actions[]='search';
         return $actions;
     }
