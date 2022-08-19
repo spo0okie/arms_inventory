a5d2ee95805b7f11014abf8fc9bbf327a0ce00a8
reviakin@yoga14 <reviakin@gmail.com>
2022-08-19 10:34:28 +0500

    Вывод списка ACL

--- views/aces/list.php
+++ views/aces/list.php
@@ -9,8 +9,12 @@ use yii\helpers\Html;
 if (is_array($models) && count($models)) {
 	echo '<h4>Имеет доступ к:</h4>';
 	foreach ($models as $model) {
-		echo $this->render('/acls/item',['model'=>$model->acl]);
-		echo '<br />';
+		if (is_object($model->acl->schedule)) {
+			echo $this->render('/schedules/item',['model'=>$model->acl->schedule,'static_view'=>true]).':';
+		} else {
+			echo 'Доступ к '.$this->render('/acls/item',['model'=>$model->acl]).':';
+		}
+		echo '<p>'.$this->render('/aces/objects',['model'=>$model]).'</p>';
 	}
 	echo '<br />';
 }
