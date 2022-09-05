e4d85ff45b6e408ef14accc58ccb3beb03bcd710
reviakin@yoga14 <reviakin@gmail.com>
2022-09-05 13:11:16 +0500

    Убирает лишние слеши

--- views/contracts/item-state.php
+++ views/contracts/item-state.php
@@ -13,8 +13,10 @@ if (is_object($model)) { ?>
     <?php if ($model->state_id) { ?>
         <span class="contract_state <?= $model->state->code?>" title="<?= $model->state->descr ?>"><?= $model->state->name?></span>
     <?php }
-    if (strlen($history[0])) { ?>
-        <span class="contract_history_preview" title="<?= html_entity_decode($history[0]) ?>"><?= ($model->state_id)?('//'.$history[0]):$history[0] ?> </span>
+    if (strlen($history[0])) {
+    	$preview=ltrim($history[0],"\t\n\r\0\x0b\\/");
+    	?>
+        <span class="contract_history_preview" title="<?= html_entity_decode($history[0]) ?>"><?= ($model->state_id)?('//'.$preview):$preview ?> </span>
     <?php } ?>
     </span>
 <?php }
