d776c8f8ae4bf214547f03fecd49606703a9e4fb
reviakin@yoga14 <reviakin@gmail.com>
2022-09-18 13:06:39 +0500

    Собственно вынес сюда информацию

---
+++ views/site/_about.php
@@ -0,0 +1,12 @@
+
+<div class="site-about p-5">
+	<h1><i class="fas fa-dice-d6"></i> Инвентаризация</h1>
+	<p class="lead">База данных софта, железа и лицензий</p>
+	<ul>
+		<li><a href="https://github.com/spo0okie/arms_inventory">github repository</a> </li>
+
+		<li><a href="https://github.com/spo0okie/arms_inventory/commits/master">dev history</a> </li>
+
+		<li><a href="https://github.com/spo0okie/arms_inventory/issues">issues list</a> </li>
+	</ul>
+</div>
