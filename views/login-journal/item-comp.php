4341b885148ad9eb2b6be0e3e4cfc1b58e185c7b
reviakin@yoga14 <reviakin@gmail.com>
2022-09-29 09:34:32 +0500

    правки отображения журнала входов

--- views/login-journal/item-comp.php
+++ views/login-journal/item-comp.php
@@ -7,5 +7,11 @@ use yii\widgets\DetailView;
 /* @var $model app\models\LoginJournal */
 /* @var $name string */
 
-if (is_object($model))
-	echo $this->render('/comps/item',['model'=>$model->comp,'name'=>$model->compName.' ('.$model->age.')']);
+if (is_object($model)) {
+	$name=$model->compName.' ('.$model->age.')';
+	if (is_object($model->comp))
+		echo $this->render('/comps/item',['model'=>$model->comp,'name'=>$name]);
+	else
+		echo $name;
+}
+
