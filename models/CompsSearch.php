5cdb1e26fa678ab3c7fd792e5acdef6cd14ff31c
reviakin@yoga14 <reviakin@gmail.com>
2022-10-17 12:59:15 +0500

    validate err

--- models/CompsSearch.php
+++ models/CompsSearch.php
@@ -101,7 +101,7 @@ class CompsSearch extends Comps
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
             // $query->where('0=1');
-            return $dataProvider;
+            //return $dataProvider;
         }
 
 	
@@ -111,7 +111,7 @@ class CompsSearch extends Comps
 
         $query->andFilterWhere(['or like', 'concat(IFNULL(domains.name,""),"\\\\",comps.name)', yii\helpers\StringHelper::explode($this->name,'|',true,true)])
             ->andFilterWhere(['or like', 'raw_version', \yii\helpers\StringHelper::explode($this->raw_version,'|',true,true)])
-			->andFilterWhere(['or like', 'ip', \yii\helpers\StringHelper::explode($this->ip,'|',true,true)])
+			->andFilterWhere(['or like', 'comps.ip', \yii\helpers\StringHelper::explode($this->ip,'|',true,true)])
 			->andFilterWhere(['or like', 'comps.mac', \yii\helpers\StringHelper::explode($this->mac,'|',true,true)])
             ->andFilterWhere(['or like', 'arms.num', \yii\helpers\StringHelper::explode($this->arm_id,'|',true,true)])
 			->andFilterWhere(['or like', 'comment', \yii\helpers\StringHelper::explode($this->comment,'|',true,true)])
