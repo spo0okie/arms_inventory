bce756757c31c99f1ae00c65a0e092ea55c5f65d
reviakin@yoga14 <reviakin@gmail.com>
2023-05-11 15:05:08 +0500

    fix #92

---
+++ migrations/m230511_094545_alter_table_login_journal.php
@@ -0,0 +1,61 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230511_094545_alter_table_login_journal
+ */
+class m230511_094545_alter_table_login_journal extends Migration
+{
+	
+	
+	function addColumnIfNotExist($table,$column,$type,$index=false)
+	{
+		$tableSchema = $this->db->getTableSchema($table);
+		if (!isset($tableSchema->columns[$column])) {
+			$this->addColumn($table,$column,$type);
+			if ($index) $this->createIndex("idx-$table-$column",$table,$column);
+			
+		}
+	}
+	
+	function dropColumnIfExist($table,$column)
+	{
+		$tableSchema = $this->db->getTableSchema($table);
+		if (isset($tableSchema->columns[$column])) {
+			$this->dropColumn($table,$column);
+		}
+	}
+	
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+		$this->addColumnIfNotExist('login_journal','type',$this->integer()->defaultValue(0));
+		
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+		$this->dropColumnIfExist('login_journal','type');
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230511_094545_alter_table_login_journal cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
