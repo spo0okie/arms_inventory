37ee3ff094b5a60a32388358c5c6fdf698cdc630
reviakin@yoga14 <reviakin@gmail.com>
2023-05-19 20:23:46 +0500

    Url helper fix

--- helpers/Url.php
+++ helpers/Url.php
@@ -3,7 +3,7 @@
 //Эта штука должна переопределить поведение базового хелпера УРЛ,
 //задача в том что при CORS запросах мы должны отдавать абсолютный урл
 //т.к. относительный будет относительно origin что ломает путь
-// схема обхода уперта тут: https://stackoverflow.com/questions/48299856/yii2-make-absolutes-url-default-on-application
+//схема обхода уперта тут: https://stackoverflow.com/questions/48299856/yii2-make-absolutes-url-default-on-application
 
 namespace yii\helpers;
 
