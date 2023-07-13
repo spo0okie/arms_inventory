7fe7ae36978e452d64f7cfe0f4054c0c5c72c953
reviakin@yoga14 <reviakin@gmail.com>
2023-07-13 12:17:47 +0500

    ip size > 512

--- models/Techs.php
+++ models/Techs.php
@@ -373,8 +373,9 @@ class Techs extends ArmsModel
 
 			[['url', 'comment','updated_at','history','hw','specs','external_links'], 'safe'],
 			[['inv_num', 'sn','installed_pos','installed_pos_end'], 'string', 'max' => 128],
-			
-			[['ip', 'mac'], 'string', 'max' => 255],
+	
+			[['ip'], 'string', 'max' => 512],
+			[['mac'], 'string', 'max' => 255],
 			['ip', function ($attribute, $params, $validator) {
 				\app\models\NetIps::validateInput($this,$attribute);
 			}],
