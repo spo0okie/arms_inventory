ce89b9f83e912817a528d83f46b384c72405d7e5
reviakin@yoga14 <reviakin@gmail.com>
2023-07-19 14:25:34 +0500

    fix #77

--- components/assets/DynaGridWidgetAsset/js/resize-columns.js
+++ components/assets/DynaGridWidgetAsset/js/resize-columns.js
@@ -1,19 +1,29 @@
 function persistResizeColumn(key,val) {
+    let debug=false;
     let tokens=key.split('-');
-    if (!tokens.length) return; //???must be like kv-2364-arms-index-place
+    if (!tokens.length) {
+        if (debug) console.log("persistResizeColumn: incorrect tokens length: "+key);
+        return;
+    } //???must be like kv-2364-arms-index-place
 
-    if (tokens[0]!=='kv') return; //
+    if (tokens[0]!=='kv') {
+        if (debug) console.log("persistResizeColumn: incorrect tokens prefix");
+        return;
+    } //
     tokens.splice(0,1); //remove 'kv'
 
     let userId=tokens[0];
-    if (userId=='guest') return; //not saving for guests
+    if (userId=='guest') {
+        if (debug) console.log("persistResizeColumn: guest user ignored: "+userId);
+        return;
+    } //not saving for guests
     tokens.splice(0,1); //remove 'userId'
 
     let column=tokens[tokens.length-1];
     tokens.splice(tokens.length-1,1); //remove 'column'
 
     let table=tokens.join('-');
-    //console.log('SET',table,column,userId,val);
+    if (debug) console.log('SET',table,column,userId,val);
 
     $.ajax({
         url: "/web/ui-tables-cols/set"+
