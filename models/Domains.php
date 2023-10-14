b56669c11ea1c73287ce9c5dee5ab13f5b9e462a
reviakin@yoga14 <reviakin@gmail.com>
2023-10-14 16:55:50 +0500

    doc upd

--- models/Domains.php
+++ models/Domains.php
@@ -2,7 +2,8 @@
 
 namespace app\models;
 
-use Yii;
+
+use yii\db\ActiveRecord;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -13,9 +14,8 @@ use yii\helpers\ArrayHelper;
  * @property string $fqdn FQDN
  * @property string $comment Комментарий
  *
- * @property CompNames[] $compNames
  */
-class Domains extends \yii\db\ActiveRecord
+class Domains extends ActiveRecord
 {
     /**
      * @inheritdoc
@@ -51,34 +51,53 @@ class Domains extends \yii\db\ActiveRecord
             'comment' => 'Комментарий',
         ];
     }
-
-    /**
-     * @return \yii\db\ActiveQuery
-     */
-    public function getCompNames()
-    {
-        return $this->hasMany(CompNames::className(), ['domain_id' => 'id']);
-    }
+    
 
     public static function fetchNames(){
         $list= static::find()
             ->select(['id','name'])
             ->all();
-        return \yii\helpers\ArrayHelper::map($list, 'id', 'name');
+        return ArrayHelper::map($list, 'id', 'name');
     }
-
+	
+	/**
+	 * ID домена по имени
+	 * @param $name
+	 * @return int|null
+	 */
 	public static function findByName($name){
 		$domain=static::find()->where(['LOWER(name)'=>mb_strtolower($name)])->one();
 		if (!is_object($domain)) return null;
 		/** @var Domains $domain */
 		return $domain->id;
 	}
-
+	
+	/**
+	 * ID домена по FQDN
+	 * @param $name
+	 * @return int|null
+	 */
 	public static function findByFQDN($name){
 		$domain=static::find()->where(['LOWER(fqdn)'=>mb_strtolower($name)])->one();
 		if (!is_object($domain)) return null;
 		/** @var Domains $domain */
 		return $domain->id;
+	}
+	
+	/**
+	 * ID домена по имени или FQDN
+	 * @param $name
+	 * @return int|null
+	 */
+	public static function findByAnyName($name) {
+		if (mb_strpos($name,'.')) { //ищем точно по FQDN!
+			return static::findByFQDN($name);
+		}
+		//имя без точек, пробуем найти его по имени
+    	if (!is_null($domain_id=static::findByName($name))) return $domain_id;
+    	//ну либо это домен первого уровня
+    	return static::findByFQDN($name);
+			
 	}
 	
 	/**
