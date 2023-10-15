9d0b618042331f7ded671906c59e753ec0b8ef4d
reviakin@yoga14 <reviakin@gmail.com>
2023-10-15 09:49:23 +0500

    controller unify

--- modules/api/controllers/LicKeysController.php
+++ modules/api/controllers/LicKeysController.php
@@ -3,25 +3,24 @@
 namespace app\modules\api\controllers;
 
 
-class LicKeysController extends \yii\rest\ActiveController
+use app\controllers\ArmsBaseController;
+use app\models\Comps;
+use yii\web\NotFoundHttpException;
+
+class LicKeysController extends ArmsBaseController
 {
 
 	public $modelClass='app\models\LicKeys';
 
-	public function actions()
-	{
-		return ['view'];
-	}
-
-	public function actionView($product_id,$comp_name){
-		$modelClass = $this->modelClass;
-		$comp=\app\models\Comps::find()->where(['name' => strtoupper($comp_name)])->one();
+	public function actionSearch($product_id,$comp_name){
+		/** @var Comps $comp */
+		$comp= Comps::find()->where(['name' => strtoupper($comp_name)])->one();
 		if ($comp === null)
-			throw new \yii\web\NotFoundHttpException("Comp with name '$comp_name' not found");
+			throw new NotFoundHttpException("Comp with name '$comp_name' not found");
 
 		$arm=$comp->arm;
 		if ($arm === null)
-			throw new \yii\web\NotFoundHttpException("Comp with name '$comp_name' not attached to any ARM");
+			throw new NotFoundHttpException("Comp with name '$comp_name' not attached to any ARM");
 
 		foreach ($arm->licKeys as $key) {
 			foreach ($key->licItem->licGroup->soft_ids as $soft_id) if ($soft_id == $product_id) {
@@ -29,7 +28,7 @@ class LicKeysController extends \yii\rest\ActiveController
 			}
 		}
 
-		throw new \yii\web\NotFoundHttpException("ARM with comp_name '$comp_name' got no license keys for product #$product_id");
+		throw new NotFoundHttpException("ARM with comp_name '$comp_name' got no license keys for product #$product_id");
 	}
 
 }
