b076bf623a0d3a2d97e348f9dd53f09faeea8c45
reviakin@yoga14 <reviakin@gmail.com>
2023-10-22 21:37:27 +0500

    controller unify

--- controllers/OrgInetController.php
+++ controllers/OrgInetController.php
@@ -2,15 +2,7 @@
 
 namespace app\controllers;
 
-use app\models\OrgPhones;
-use Yii;
 use app\models\OrgInet;
-use yii\data\ActiveDataProvider;
-use yii\helpers\Url;
-use yii\web\Controller;
-use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
-use yii\web\Response;
 
 /**
  * OrgInetController implements the CRUD actions for OrgInet model.
