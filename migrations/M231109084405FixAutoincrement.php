e5be88b1b2b77f4d540d4a56e4d48423c0940cd2
reviakin@yoga14 <reviakin@gmail.com>
2023-11-09 14:28:42 +0500

    table fixes

--- migrations/M231109084405FixAutoincrement.php
+++ migrations/M231109084405FixAutoincrement.php
@@ -14,8 +14,9 @@ class M231109084405FixAutoincrement extends Migration
      */
     public function safeUp()
     {
-    	$this->execute('update hw_ignore set id=max(id)+1 where id=0');
+    	$this->execute('set @max_id=(select max(id) from hw_ignore); update hw_ignore set id=@max_id+1 where id=0;');
 		$this->alterColumn('hw_ignore','id',$this->integer()->append(' AUTO_INCREMENT'));
+		$this->execute('set @max_id=(select max(id) from tech_states); update tech_states set id=@max_id+1 where id=0;');
 		$this->alterColumn('tech_states','id',$this->integer()->append(' AUTO_INCREMENT'));
     }
 
