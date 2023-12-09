595f7e8ffc22291aab1a74c68ce650402fed7452
reviakin@yoga14 <reviakin@gmail.com>
2023-12-09 17:38:25 +0500

    week description changes

--- views/schedules/ttip.php
+++ views/schedules/ttip.php
@@ -1,13 +1,11 @@
 <?php
 
-use yii\helpers\Html;
-
 /* @var $this yii\web\View */
 /* @var $model app\models\Schedules */
 
 ?>
 <div class="schedules-ttip ttip-card">
-	<?= $this->render('week',['model'=>$model])?>
+	<?= $this->render('week-description',['model'=>$model])?>
 	<?= $this->render('7days',['model'=>$model])?>
 	<?= $this->render('services',['model'=>$model])?>
 </div>
