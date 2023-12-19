711c2d7a054742112d32f1d47546e19fce83bd6f
reviakin@yoga14 <reviakin@gmail.com>
2023-12-19 19:25:01 +0500

    count added

--- views/materials/groups-columns.php
+++ views/materials/groups-columns.php
@@ -2,7 +2,11 @@
 
 /* Колонки для Материалы по кучкам Помещение/Тип
  */
+
+use app\helpers\ArrayHelper;
+use app\models\Materials;
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $groupBy string */
@@ -28,14 +32,15 @@ return  [
 					'name'=>$data['model']]
 				);
 			} else {
+				$link='/error';
 				if ($groupBy=='type')
 					$link=['materials/index','MaterialsSearch[type_id]'=>$data['type_id'],'MaterialsSearch[places_id]'=>$data['place_id']];
 				if ($groupBy=='name')
 					$link=['materials/index','MaterialsSearch[model]'=>$data['name'],'MaterialsSearch[places_id]'=>$data['place_id']];
 				return '<span class="material-item cursor-default" qtip_ajxhrf="' .
-					\yii\helpers\Url::to([
+					Url::to([
 						'/materials/ttips',
-						'ids' => implode(',', \app\helpers\ArrayHelper::getColumn($data['models'], 'id')),
+						'ids' => implode(',', ArrayHelper::getColumn($data['models'], 'id')),
 						'hide_places' => 1,
 						'hide_usages' => 1
 					]) . '">' .
@@ -51,15 +56,15 @@ return  [
 			$rest=0;
 			$models=$data['models'];
 			/**
-			 * @var $models \app\models\Materials[]
+			 * @var $models Materials[]
 			 */
-			\app\helpers\ArrayHelper::multisort($models,'rest');
-
+			ArrayHelper::multisort($models,'rest');
+			
 			foreach ($models as $model)
 				$rest+=$model->rest;
 			
 			$model=$models[0];
-
+			
 			$link=[];
 			if (count($models)==1) {
 				//если у нас 1 материал то ссылка будет прямо на него
@@ -72,13 +77,51 @@ return  [
 					$link=['materials/index','MaterialsSearch[model]'=>$data['name'],'MaterialsSearch[places_id]'=>$data['place_id']];
 			}
 			return '<span class="material-item cursor-default" qtip_ajxhrf="'.
-				\yii\helpers\Url::to([
+				Url::to([
 					'/materials/ttips',
-					'ids'=>implode(',',\app\helpers\ArrayHelper::getColumn($models,'id')),
+					'ids'=>implode(',', ArrayHelper::getColumn($models,'id')),
 					'hide_places'=>1,
 					'hide_usages'=>1
 				]).'">'.
 				Html::a($rest.' '. $model->type->units,$link).
+				'</span>';
+		}
+	],
+	[
+		'attribute'=>'count',
+		'label'=>'Поступило',
+		'value' => function($data) use($renderer,$groupBy){
+			$count=0;
+			$models=$data['models'];
+			/**
+			 * @var $models Materials[]
+			 */
+			ArrayHelper::multisort($models,'rest');
+			
+			foreach ($models as $model)
+				$count+=$model->count;
+			
+			$model=$models[0];
+			
+			$link=[];
+			if (count($models)==1) {
+				//если у нас 1 материал то ссылка будет прямо на него
+				$link=['materials/view','id'=>$model];
+			} else {
+				//иначе на поиск
+				if ($groupBy=='type')
+					$link=['materials/index','MaterialsSearch[type_id]'=>$data['type_id'],'MaterialsSearch[places_id]'=>$data['place_id']];
+				if ($groupBy=='name')
+					$link=['materials/index','MaterialsSearch[model]'=>$data['name'],'MaterialsSearch[places_id]'=>$data['place_id']];
+			}
+			return '<span class="material-item cursor-default" qtip_ajxhrf="'.
+				Url::to([
+					'/materials/ttips',
+					'ids'=>implode(',', ArrayHelper::getColumn($models,'id')),
+					'hide_places'=>1,
+					'hide_usages'=>1
+				]).'">'.
+				Html::a($count.' '. $model->type->units,$link).
 				'</span>';
 		}
 	],
