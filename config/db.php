2b70b04f9693f9934e0b8de1d56dbe0eb46eb59b
reviakin@yoga14 <reviakin@gmail.com>
2024-03-23 21:08:58 +0500

    syntax fix

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
 	'dsn' => 'mysql:host=127.0.0.1;dbname=arms', 'username' => 'root',    'password' => '',
 	'on afterOpen' => function($event) {
-        $event->sender->createCommand("SET time_zone='+00:00';")->execute();
+        $event->sender->createCommand("SET time_zone='+00:00';set sql_mode='';")->execute();
 	},
     'charset' => 'utf8mb4',
 ];
