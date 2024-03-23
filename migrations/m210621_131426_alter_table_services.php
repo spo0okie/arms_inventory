117327bed6cef607a29d71ed120fd29aa8038d78
reviakin@yoga14 <reviakin@gmail.com>
2024-03-23 10:32:13 +0500

    string length fix

--- migrations/m210621_131426_alter_table_services.php
+++ migrations/m210621_131426_alter_table_services.php
@@ -1,24 +1,18 @@
 <?php
 namespace app\migrations;
-use yii\db\Migration;
+use app\migrations\arms\ArmsMigration;
 
 /**
  * Class m210621_131426_alter_table_services
  */
-class m210621_131426_alter_table_services extends Migration
+class m210621_131426_alter_table_services extends ArmsMigration
 {
 	/**
 	 * {@inheritdoc}
 	 */
 	public function safeUp()
 	{
-		$table = $this->db->getTableSchema('services');
-		if (!isset($table->columns['parent_id'])) {
-			$this->addColumn('services', 'parent_id', $this->integer()->null());
-			$this->createIndex('{{%idx-services_parent_id}}', '{{%services}}', '[[parent_id]]');
-			
-		}
-		
+		$this->addColumnIfNotExist('services', 'parent_id', $this->integer()->null(),true);
 	}
 	
 	/**
@@ -26,11 +20,7 @@ class m210621_131426_alter_table_services extends Migration
 	 */
 	public function safeDown()
 	{
-		$table = $this->db->getTableSchema('services');
-		if (isset($table->columns['parent_id'])) {
-			$this->dropColumn('services', 'parent_id');
-			$this->dropIndex('{{%idx-services_parent_id}}', '{{%services}}');
-		}
+		$this->dropColumnIfExist('services', 'parent_id');
 	}
 }
 
