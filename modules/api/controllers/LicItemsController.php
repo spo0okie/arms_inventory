4061ba59a4a49fabe8426f8cb78695105b961501
Reviakin@home <reviakin@gmail.com>
2024-12-17 15:04:36 +0500

    lics api update

--- modules/api/controllers/LicItemsController.php
+++ modules/api/controllers/LicItemsController.php
@@ -3,6 +3,8 @@
 namespace app\modules\api\controllers;
 
 use app\models\Comps;
+use app\models\Users;
+use yii\web\BadRequestHttpException;
 use yii\web\NotFoundHttpException;
 
 /**
@@ -14,24 +16,55 @@ class LicItemsController extends BaseRestController
 {
 
 	public $modelClass='app\models\LicItems';
-
-	public function actionSearch($product_id=null,$comp_name=null){
-		/** @var Comps $comp */
-		$comp= Comps::find()->where(['name' => strtoupper($comp_name)])->one();
-		if ($comp === null)
-			throw new NotFoundHttpException("Comp with name '$comp_name' not found");
-
-		$arm=$comp->arm;
-		if ($arm === null)
-			throw new NotFoundHttpException("Comp with name '$comp_name' not attached to any ARM");
-
-		foreach ($arm->licItems as $item) {
-			foreach ($item->licGroup->soft_ids as $soft_id) if ($soft_id == $product_id) {
-				return $item;
+	
+	public function actionSearch($product_id=null,$comp_name=null,$user_login=null){
+		//$modelClass = $this->modelClass;
+		
+		$licItems=[];
+		$errors=[];
+		
+		$comp=null;
+		$user=null;
+		/**
+		 * @var $comp Comps
+		 * @var $user Users
+		 */
+		
+		if (!$product_id) {
+			throw new BadRequestHttpException("No product_id passed");
+		}
+		
+		if (!$comp_name && !$user_login) {
+			throw new BadRequestHttpException("No login or computer name passed");
+		}
+		
+		if ($comp_name) {
+			try {
+				$comp=\app\controllers\CompsController::searchModel($comp_name);
+				foreach ($comp->licItems as $licItem)
+					foreach ($licItem->licGroup->soft_ids as $soft_id)
+						if ($soft_id == $product_id)
+							$licItems[]=$licItem;
+			} catch (NotFoundHttpException $e) {
+				$errors[]=$e->getMessage();
 			}
 		}
-
-		throw new NotFoundHttpException("ARM with comp_name '$comp_name' got no license keys for product #$product_id");
+		
+		if ($user_login) {
+			if (!is_object($user= Users::findByLogin($user_login)))
+				$errors[]="User '$user_login' not found";
+			else {
+				foreach ($user->licItems as $licItem)
+					foreach ($licItem->licGroup->soft_ids as $soft_id)
+						if ($soft_id == $product_id)
+							$licItems[]=$licItem;
+			}
+		}
+		
+		if (!is_object($comp) && !is_object($user))
+			throw new NotFoundHttpException(implode(',',$errors));
+		
+		return $licItems;
 	}
 
 }
--- modules/api/controllers/LicKeysController.php
+++ modules/api/controllers/LicKeysController.php
@@ -3,6 +3,8 @@
 namespace app\modules\api\controllers;
 
 use app\models\Comps;
+use app\models\Users;
+use yii\web\BadRequestHttpException;
 use yii\web\NotFoundHttpException;
 
 /**
@@ -14,24 +16,55 @@ class LicKeysController extends BaseRestController
 {
 
 	public $modelClass='app\models\LicKeys';
-
-	public function actionSearch($product_id=null,$comp_name=null){
-		/** @var Comps $comp */
-		$comp= Comps::find()->where(['name' => strtoupper($comp_name)])->one();
-		if ($comp === null)
-			throw new NotFoundHttpException("Comp with name '$comp_name' not found");
-
-		$arm=$comp->arm;
-		if ($arm === null)
-			throw new NotFoundHttpException("Comp with name '$comp_name' not attached to any ARM");
-
-		foreach ($arm->licKeys as $key) {
-			foreach ($key->licItem->licGroup->soft_ids as $soft_id) if ($soft_id == $product_id) {
-				return $key;
+	
+	public function actionSearch($product_id=null,$comp_name=null,$user_login=null){
+		//$modelClass = $this->modelClass;
+		
+		$licKeys=[];
+		$errors=[];
+		
+		$comp=null;
+		$user=null;
+		/**
+		 * @var $comp Comps
+		 * @var $user Users
+		 */
+		
+		if (!$product_id) {
+			throw new BadRequestHttpException("No product_id passed");
+		}
+		
+		if (!$comp_name && !$user_login) {
+			throw new BadRequestHttpException("No login or computer name passed");
+		}
+		
+		if ($comp_name) {
+			try {
+				$comp=\app\controllers\CompsController::searchModel($comp_name);
+				foreach ($comp->licKeys as $licKey)
+					foreach ($licKey->licItem->licGroup->soft_ids as $soft_id)
+						if ($soft_id == $product_id)
+							$licKeys[]=$licKey;
+			} catch (NotFoundHttpException $e) {
+				$errors[]=$e->getMessage();
 			}
 		}
-
-		throw new NotFoundHttpException("ARM with comp_name '$comp_name' got no license keys for product #$product_id");
+		
+		if ($user_login) {
+			if (!is_object($user= Users::findByLogin($user_login)))
+				$errors[]="User '$user_login' not found";
+			else {
+				foreach ($user->licKeys as $licKey)
+					foreach ($licKey->licItem->licGroup->soft_ids as $soft_id)
+						if ($soft_id == $product_id)
+							$licKeys[]=$licKey;
+			}
+		}
+		
+		if (!is_object($comp) && !is_object($user))
+			throw new NotFoundHttpException(implode(',',$errors));
+		
+		return $licKeys;
 	}
 
 }
