4dccd4d991f03208f350a08bd3d7424aa124bdd9
Reviakin@home <reviakin@gmail.com>
2025-05-15 16:46:57 +0500

    modal option added

--- views/layouts/item.php
+++ views/layouts/item.php
@@ -1,31 +1,19 @@
 <?php
 
 use app\components\ItemObjectWidget;
-use app\components\LinkObjectWidget;
 
 /* @var $this yii\web\View */
-/* @var $model app\models\AccessTypes */
-
-if (!isset($static_view)) $static_view=false;
-if (!isset($show_delete)) $show_delete=false;
-if (!isset($name)) $name=$model->sname;
-if (!isset($suffix)) $suffix='';
-if (!isset($class)) $class='';
+/* @var $model app\models\ArmsModel */
 
 if (!empty($model)) {
 	echo ItemObjectWidget::widget([
 		'model'=>$model,
-		'item_class'=>$class,
+		'item_class'=>$class??'',
 		//'archived_class'=>'text-decoration-line-through',
-		'name'=>$name,
-		'nameSuffix'=>$suffix,
-		'noDelete'=>!$show_delete,
-		'static'=>$static_view,
+		'name'=>$name??null,
+		'nameSuffix'=>$suffix??'',
+		'noDelete'=>!($show_delete??false),
+		'static'=>$static_view??false,
+		'modal'=>$modal??false,
 	]);
 }
-
-
-
-?>
-
-
