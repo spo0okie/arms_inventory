edd936fd6f40f2c537abb695ee69cc51eb045a59
Reviakin@home <reviakin@gmail.com>
2025-06-14 22:28:42 +0500

    clean unused files

--- tests/unit/_bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-$config = require __DIR__ . '/../../config/test-console.php';
-new yii\console\Application($config);
-print_r(Yii::$app ?
-	array_keys(Yii::$app->getComponents()) :
-	'Yii::$app не инициализирован'
-);
-*/
