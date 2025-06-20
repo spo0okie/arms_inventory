c97ffa4a3807416e53cfd8137300bd99865d132c
Reviakin@home <reviakin@gmail.com>
2025-06-20 14:51:37 +0500

    render-fix

--- views/techs/docs/passport-row.php
+++ views/techs/docs/passport-row.php
@@ -7,8 +7,8 @@
  * @var \app\models\Techs $model
  */
 ?>
-<td class="tech_type"><?= $this->render('/tech-types/item',['model'=>$model->model->type]) ?></td>
-<td class="tech_model" ><?= $this->render('/tech-models/item',['model'=>$model->model]) ?></td>
-<td class="tech_id"><?= $this->render('/techs/item',['model'=>$model]) ?></td>
+<td class="tech_type"><?= $model->model->type->renderItem($this) ?></td>
+<td class="tech_model" ><?= $model->model->renderItem($this) ?></td>
+<td class="tech_id"><?= $model->renderItem($this) ?></td>
 <td class="tech_sn"><?= $model->sn ?></td>
 <td class="tech_inv_num"><?= $model->inv_num ?></td>
