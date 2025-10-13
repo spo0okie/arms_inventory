30f47478055180642307d60f51368ea923bde65b
Reviakin@home <reviakin@gmail.com>
2025-10-13 17:38:47 +0500

    editable comment

--- views/soft/columns.php
+++ views/soft/columns.php
@@ -1,5 +1,6 @@
 <?php
 
+use kartik\editable\Editable;
 use yii\helpers\Html;
 use kartik\grid\GridView;
 
@@ -19,7 +20,33 @@ return [
 			]);
 		}
 	],
-	'comment',
+	'comment'=>[
+		'class'=>'kartik\grid\EditableColumn',
+		'editableOptions'=> [
+				'name'=>'comment',
+				'header'=>'Комментарий',
+				'format'=>Editable::FORMAT_LINK,
+				'inputType' => Editable::INPUT_TEXT,
+				'inlineSettings' => [
+					'templateBefore'=>'<div class="kv-editable-form-inline d-flex w-100 g-0 m-0"><div class="mb-2">{loading}</div>',
+				],
+				'asPopover' => false,
+				'buttonsTemplate'=>'{submit}',
+				'options' => [
+					'class' => 'w-100',
+					'placeholder'=>'Введите пояснение...',
+				],
+				'containerOptions'=>['class'=>'w-100 p-0 m-0'],
+				'contentOptions'=>['class'=>'p-0 m-0'],
+				'inputFieldConfig'=>['options'=>['class'=>'flex-grow-1']],
+				'editableValueOptions'=>['class'=>'p-0 m-0 border-0 text-start bg-transparent',],
+				'formOptions' => [
+					'action' => [
+						'/soft/editable',
+					]
+				],
+			],
+	],
 	'hitsCount',
 	'compsCount',
 	'licGroupsCount',
