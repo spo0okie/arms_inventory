7abdab9823c7916a962930df43d9712514ddd0a4
Reviakin@home <reviakin@gmail.com>
2025-12-15 18:11:00 +0500

    service comments added

--- views/org-inet/view.php
+++ views/org-inet/view.php
@@ -1,17 +1,25 @@
 <?php
 
-use yii\helpers\Html;
-use yii\widgets\DetailView;
+use app\components\TextFieldWidget;
+use app\models\OrgInet;
+use yii\helpers\Url;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\OrgInet */
-\yii\helpers\Url::remember();
+Url::remember();
 
 $static_view=false;
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => \app\models\OrgInet::$titles, 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => OrgInet::$titles, 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="org-inet-view">
 	<?= $this->render('/services/card',['model'=>$model->service ,'static_view'=>$static_view]) ?>
+	<?php if (strlen($model->service->notebook)) { ?>
+		<h4>Записная книжка:</h4>
+		<p>
+			<?= TextFieldWidget::widget(['model'=>$model->service,'field'=>'notebook']) ?>
+		</p>
+		<br />
+	<?php } ?>
 </div>
--- views/org-phones/view.php
+++ views/org-phones/view.php
@@ -1,15 +1,22 @@
 <?php
 
-use yii\helpers\Html;
-use yii\widgets\DetailView;
+use app\components\TextFieldWidget;
+use app\models\OrgPhones;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\OrgPhones */
 
 $this->title = $model->sname;
-$this->params['breadcrumbs'][] = ['label' => \app\models\OrgPhones::$title, 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => OrgPhones::$title, 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="org-phones-view">
 	<?= $this->render('card',['model'=>$model]) ?>
+	<?php if (strlen($model->service->notebook)) { ?>
+		<h4>Записная книжка:</h4>
+		<p>
+			<?= TextFieldWidget::widget(['model'=>$model->service,'field'=>'notebook']) ?>
+		</p>
+		<br />
+	<?php } ?>
 </div>
