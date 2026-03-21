2fead737d53b85523e50c36946b5bd9cf4c70e02
Reviakin@home <reviakin@gmail.com>
2026-03-21 12:39:21 +0500

    generators start

---
+++ generation/generators/GeneratorInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\generation\generators;
+
+interface GeneratorInterface
+{
+    /**
+     * Генерация значения атрибута.
+     *
+     * Требования:
+     * - не бросает исключения в нормальном сценарии
+     * - не содержит бизнес-логики
+     * - не обращается к БД
+     */
+    public function generate(array $params): mixed;
+}
---
+++ generation/generators/StringGenerator.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace app\generation\generators;
+
+class StringGenerator implements GeneratorInterface
+{
+    public function generate(array $params): mixed
+    {
+		//если нужен пустой атрибут
+		if ($params['empty']??false) {
+			//если атрибут может быть null
+			if ($params['nullable']??false) return null;
+			
+			return '';
+		}
+
+        $min = $params['min'] ?? 5;
+        $max = $params['max'] ?? 20;
+
+        $length = random_int($min, $max);
+
+        return $this->randomString($length);
+    }
+
+    protected function randomString(int $length): string
+    {
+        // максимально простой и быстрый вариант
+        $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+
+        $result = '';
+        $maxIndex = strlen($chars) - 1;
+
+        for ($i = 0; $i < $length; $i++) {
+            $result .= $chars[random_int(0, $maxIndex)];
+        }
+
+        return $result;
+    }
+}
