808b0ed321f09d1fbd66e56e11921177067fabba
Reviakin@home <reviakin@gmail.com>
2026-07-13 15:02:05 +0500

    legacy css cleanup

--- views/segments/_form.php
+++ views/segments/_form.php
@@ -9,26 +9,6 @@ use yii\bootstrap5\ActiveForm;
 /* @var $model app\models\Segments */
 /* @var $form yii\widgets\ActiveForm */
 if (!isset($modalParent)) $modalParent=null;
-$defaultCss=[
-	'open',
-	'closed',
-	'common',
-	'gw_close2open',
-	'gw_open2close',
-	'ext',
-	'ext_dmz',
-	'int_dmz',
-	'open_dmz',
-	'closed_dmz',
-	'guest_dmz',
-	'client_vpn',
-	'intersite_vpn',
-	'it_lan',
-	'voip',
-	'prn',
-	'skud',
-	'mgmt'
-];
 ?>
 
 <div class="segments-form">
@@ -46,15 +26,6 @@ $defaultCss=[
 			<?= $form->field($model,'links')->textAutoresize() ?>
 		</div>
 	</div>
-	<div class="d-flex flex-row flex-wrap">
-		<span class="m-1">Готовые коды: </span>
-		<?php
-		foreach ($defaultCss as $css) {
-			echo '<span class="segment_'.$css.' m-1 px-1"><a href="#" onclick="$(\'#segments-code\').val(\'segment_'.$css.'\')">segment_'.$css.'</a></span>';
-		}
-		?>
-	</div>
-
 	<?= $form->field($model, 'history')->text() ?>
 
     <div class="form-group">
